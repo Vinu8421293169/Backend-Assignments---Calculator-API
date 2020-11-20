e6f96b70a4916f1389e7f25d26f85e7ddcaf9cc6
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-11-20 15:36:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,29 +19,28 @@ app.get('/',(req,res)=>{
 
 
 app.post('/add',(req,res)=>{
-    const num1=parseFloat(req.body.num1);
-    const num2=parseFloat(req.body.num2);
+    const num1=req.body.num1;
+    const num2=req.body.num2;
     result=num1+num2;
     res.send({status: "success",message: result<-1000000?"Underflow":result>1000000?"Overflow":"the sum of given two numbers",sum:result});
 })
 
 app.post('/sub',(req,res)=>{
-    const num1=parseFloat(req.body.num1);
-    const num2=parseFloat(req.body.num2);
+    const num1=req.body.num1;
+    const num2=req.body.num2;
     result=num1-num2;
     res.send({status: "success",message: result<-1000000?"Underflow":result>1000000?"Overflow":"the sum of given two numbers",sum:result});
 })
 
 app.post('/multiply',(req,res)=>{
-    const num1=parseFloat(req.body.num1);
-    const num2=parseFloat(req.body.num2);
-    result=num1*num2;
-    res.send({status: "success",message: result<-1000000?"Underflow":result>1000000?"Overflow":"The product of given numbers",sum:result});
+    const num1=req.body.num1;
+    const num2=req.body.num2;
+    res.send({status: "success",message: result<-1000000?"Underflow":result>1000000?"Overflow":"The product of given numbers",sum:num1*num2});
 })
 
 app.post('/divide',(req,res)=>{
-    const num1=parseFloat(req.body.num1);
-    const num2=parseFloat(req.body.num2);
+    const num1=req.body.num1;
+    const num2=req.body.num2;
     result=num1/num2;
     res.send({status: num2===0?"error":"success",message: num2===0?"Cannot divide by zero":"The division of given numbers",sum:result});
 })
