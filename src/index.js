c1340ca9a939c4e6e6c4ae803bb0ac6c7cbfa8e3
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-11-20 15:24:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // your code goes here
 
-
+app.get('/',(req,res)=>{
+    res.status(200).send('Hello world!');
+})
 
 
 app.post('/add',(req,res)=>{
