a6079b01dd6dd5b7f1bde86a0ea5b6cfcd56c1a0
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-11-20 15:46:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,27 +22,27 @@ app.post('/add',(req,res)=>{
     const num1=req.body.num1;
     const num2=req.body.num2;
     result=num1+num2;
-    res.send({status: "success",message: result<-1000000?"Underflow":result>1000000?"Overflow":"the sum of given two numbers",sum:result});
+    res.send({status: "success",message:typeof num1==='string'|| typeof num2=== "string"?"Invalid data types":result<-1000000?"Underflow":result>1000000?"Overflow":"the sum of given two numbers",sum:result});
 })
 
 app.post('/sub',(req,res)=>{
     const num1=req.body.num1;
     const num2=req.body.num2;
     result=num1-num2;
-    res.send({status: "success",message: result<-1000000?"Underflow":result>1000000?"Overflow":"the sum of given two numbers",sum:result});
+    res.send({status: "success",message:typeof num1==='string'|| typeof num2=== "string"?"Invalid data types": result<-1000000?"Underflow":result>1000000?"Overflow":"the sum of given two numbers",sum:result});
 })
 
 app.post('/multiply',(req,res)=>{
     const num1=req.body.num1;
     const num2=req.body.num2;
-    res.send({status: "success",message: result<-1000000?"Underflow":result>1000000?"Overflow":"The product of given numbers",sum:num1*num2});
+    res.send({status: "success",message:typeof num1==='string'|| typeof num2=== "string"?"Invalid data types": result<-1000000?"Underflow":result>1000000?"Overflow":"The product of given numbers",sum:num1*num2});
 })
 
 app.post('/divide',(req,res)=>{
     const num1=req.body.num1;
     const num2=req.body.num2;
     result=num1/num2;
-    res.send({status: num2===0?"error":"success",message: num2===0?"Cannot divide by zero":"The division of given numbers",sum:result});
+    res.send({status: num2===0?"error":"success",message:typeof num1==='string'|| typeof num2=== "string"?"Invalid data types": num2===0?"Cannot divide by zero":"The division of given numbers",sum:result});
 })
 
 
